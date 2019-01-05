63a150be088fb15a039dfff63340558836c25527
Oleg <oyapparov@gmail.com>
2019-01-05 13:14:30 +0100

    Fix indentation

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -4,29 +4,29 @@ const commonConfig = require('./webpack.common.js');
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 
 module.exports = webpackMerge(commonConfig, {
-    devtool: 'source-map',
+  devtool: 'source-map',
 
-    externals: {
-        'react': 'React',
-        'react-dom': 'ReactDOM'
-    },
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM'
+  },
 
-    performance: {
-        hints: "warning"
-    },
+  performance: {
+    hints: "warning"
+  },
 
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true,
-            mangle: {
-                screw_ie8: true,
-                keep_fnames: true
-            },
-            compress: {
-                warnings: false,
-                screw_ie8: true
-            },
-            comments: false
-        })
-    ]
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      mangle: {
+        screw_ie8: true,
+        keep_fnames: true
+      },
+      compress: {
+        warnings: false,
+        screw_ie8: true
+      },
+      comments: false
+    })
+  ]
 });
