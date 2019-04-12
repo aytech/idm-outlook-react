578b047103f81f3a0858902d42fa1d5018c00d46
Jakub Kovarik <jakub.kovarik@infor.com>
2019-04-12 15:08:12 +0200

    Unified header component, extracted landing page component, simplified app component
    Removing deprecated spinner type
    
    Fixes for langing page

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,5 @@
 export const Constants = {
-  APP_TITLE: 'Document Management for Outlook'
+  APP_TITLE: 'Document Management for Outlook',
+  HEADER_LOGO: 'assets/logo.png',
+  PLUGIN_BROCHURE_LINK: 'https://www.infor.com/content/brochures/document-mgt.pdf/'
 };
